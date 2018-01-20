3ea92ccf6268232e4d299ea344ee30532e99f521
Renon Stewart <s.renon@gmail.com>
2018-01-20 17:45:59 -0500

    Update license

--- Helper/Data.php
+++ Helper/Data.php
@@ -1,12 +1,7 @@
 <?php
 /**
- * MagePal_AdminSalesOrderViewButton Magento component
- *
- * @category    MagePal
- * @package     MagePal_AdminSalesOrderViewButton
- * @author      MagePal Team <info@magepal.com>
- * @copyright   MagePal (http://www.magepal.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Copyright © MagePal LLC. All rights reserved.
+ * See COPYING.txt for license details.
  */
 
 namespace MagePal\CustomShippingRate\Helper;
